835eb7cbb13074a08ff8ad3cc3b64ae10375d1e7
RohithSachdeva <rsachdeva@ucdavis.edu>
2020-08-06 10:00:39 -0700

    90% done... Still playing/learning/fixing

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,34 +1,68 @@
 const express = require('express');
 const userDb = require('./userDb');
+const e = require('express');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser, (req, res) => {
+  User.insert(req.body.name)
+  .then(item => {
+    res.status(201).json(item)
+  })
+  .catch(error => {
+    res.status(500).json({message: "Error occurred"})
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+//NOT DONE 
+router.post('/:id/posts', validatePost, (req, res) => {
+  
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  User.get()
+  .then(item => {
+    res.status(200).json(item)
+  })
+  .catch(error => {
+    res.status(500).json({message: "Error occurred"})
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user)
 });
 
 router.get('/:id/posts', (req, res) => {
-  // do your magic!
+  User.getUserPosts(req.params.id)
+  .then(item => {
+    res.status(200).json(item)
+  })
+  .catch(error => {
+    res.status(500).json({message: "Error occurred"})
+  })
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  User.remove(req.params.id)
+  .then(res.status(200).end())
+  .catch(error => {
+    res.status(500).json({message: "Error occurred"})
+  })
 });
 
+// NOT DONE 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  User.update(req.params.id, req.body.name)
+  .then(newItem => {
+    if(newItem){
+      User.getById(req.params.id)
+      .then(item => {
+        res.status(200).json(item)
+      })
+
+    }
+  })
 });
 
 //custom middleware
@@ -37,16 +71,37 @@ function validateUserId(req, res, next) {
   const id = req.params.id
   userDb.getById(id)
   .then(item => {
-    if
+    if(item){
+      console.log(req.user)
+      req.user = item;
+      next();
+    } else {
+      res.status(400).json( {message: "User not found"})
+    }
+  })
+  .catch(error => {
+    res.status(500).json({message: "Error occurred"})
   })
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if(!req.body) {
+    res.status(400).json({message: "data required"})
+  } else if(!req.body.name) {
+    res.status(400).json({message:"Name required"})
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if (!req.body){ 
+    res.status(400).json({message: "Data required"})
+  } else if(!req.body.text) {
+    res.status(400).json( { message: "Text required"})
+  } else {
+    next();
+  }
 }
 
 module.exports = router;
